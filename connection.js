1a0c096e2b48141ba2e48e35fddb80ff5798345c
Afrin Khan <35771504+saddaf88@users.noreply.github.com>
2021-06-03 22:12:52 +0200

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,9 +2,9 @@ var mysql = require('mysql2');
 
 var con = mysql.createConnection({
 	host: "mysql.hrz.tu-chemnitz.de",
-	user: "SMS_test_rw",
-	password: "chu0Ulei4i",
-	database: 'SMS_test'
+	user: "SMS_DB_rw",
+	password: "ahK4jeezai",
+	database: 'SMS_DB'
 });
 
 // con.connect(function (err) {
