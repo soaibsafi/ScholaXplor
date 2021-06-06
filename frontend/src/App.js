f8729cb8d3522d239f8e2eb237d7ad6713e79102
Saddaf Afrin Khan <saddaf88@gmail.com>
2021-06-06 17:39:55 +0200

    (SETUP) Page Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+ import login from './view/login';
+
+
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <div class="container py-4">
+            <div class="row">
+
+              <Route path="/" exact component={login} />
+              {/*<Route path="/studetnForm" component={studetnForm} />*/}
+              {/*<Route path="/edit/:employeeId" component={Edit} />*/}
+
+            </div>
+          </div>
+
+        </div>
+      </Router>
   );
 }
 
 export default App;
+
+
+
---
+++ frontend/src/view/login.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+class login extends React.Component{
+  render(){
+    return(
+      <div style={{backgroundColor: "DodgerBlue"}}>
+
+
+      </div>
+    )
+  }
+}
+
+
+
+export default login;
