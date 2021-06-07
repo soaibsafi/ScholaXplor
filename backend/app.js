497fad83a8e21f59e51b13437f756fdae3130df0
soaibsafi <soaib.safi@gmail.com>
2021-06-07 21:15:08 +0200

    :sparkles: Update- JWT Auth final

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const router = require('./src/routes/auth.routes');
-app.use('/', router);
+// const router = require('./src/routes/auth.routes');
+// app.use('/', router);
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to School Management System" });
 });
 
- require("./src/routes/example.routes")(app);
- require('./src/routes/user.routes')(app)
- //require('./src/routes/auth.routes')(app)
+// Import All router
+require("./src/routes/user.routes")(app);
+require("./src/routes/auth.routes")(app);
+
+
 // set port, listen for requests
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
