fc2cd48f7c50ead5cd76c28d32ebe073ac9f4e14
masyam <masyam.cse@gmail.com>
2021-06-08 20:07:08 +0200

    init commit

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,8 @@ app.get("/", (req, res) => {
 // Import All router
 require("./src/routes/user.routes")(app);
 require("./src/routes/auth.routes")(app);
+require("./src/routes/class.routes")(app);
+require("./src/routes/subject.routes")(app);
 
 
 // set port, listen for requests
