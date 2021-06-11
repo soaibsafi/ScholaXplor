dcee95603cc56639f27217e19004f75e098d828e
soaibsafi <soaib.safi@gmail.com>
2021-06-11 15:26:19 +0200

    Add - Query parameter user

--- backend/src/routes/subject.routes.js
+++ backend/src/routes/subject.routes.js
@@ -3,11 +3,23 @@ const userMiddleware = require("../middleware/authjwt");
 module.exports = (app) => {
   const subject = require("../controllers/subject.controller");
 
-  app.get("/subjects/:classId", userMiddleware.isAdminLoggedIn,  subject.getAllSubjectByClass);
-  app.get("/subject-average/:subjectId", userMiddleware.isTeacherLoggedIn,  subject.getAverageGradeBySubjectId);
-  app.get('/pupil-subjects/:pupilId', userMiddleware.isTeacherLoggedIn, subject.getAllSubjectByPupilId)
-  app.get('/pupil-average/', subject.getAverageGradePupilSUbject)
-  app.get('/test-grades/', subject.getAllGradeBySubjectPupilId)
+  app.get(
+    "/subjects/:classId",
+    userMiddleware.isAdminLoggedIn,
+    subject.getAllSubjectByClass
+  );
+  app.get(
+    "/subject-average/:subjectId",
+    userMiddleware.isTeacherLoggedIn,
+    subject.getAverageGradeBySubjectId
+  );
+  app.get(
+    "/pupil-subjects/:pupilId",
+    userMiddleware.isTeacherLoggedIn,
+    subject.getAllSubjectByPupilId
+  );
+  app.get("/pupil-average/", subject.getAverageGradePupilSUbject); //pupil-average/?sid=s20&pid=u1
+  app.get("/test-grades/", subject.getAllGradeBySubjectPupilId); // test-grades/?sid=s20&pid=u2
   app.put("/subject/:subjectId", subject.updateSubjectById);
   app.delete("/subject/:subjectId", subject.deleteSubjectById);
   app.post("/subject/", subject.createSubject);
